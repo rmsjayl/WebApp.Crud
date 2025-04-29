e4de66c2044adfd66a9c5ce2e543b1c6daeb0e72
rmsjayl <jaylord.ramos016@gmail.com>
2025-04-29 10:56:31 +0800

    [BE] Enhancement - Added new migrations

---
+++ Migrations/20250429025451_Changed access modifier to PUBLIC.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Crud.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedaccessmodifiertoPUBLIC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CellPhoneNumber",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "University",
+                table: "Students",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CellPhoneNumber",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "University",
+                table: "Students");
+        }
+    }
+}
