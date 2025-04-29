8546082e3b681eff8ac03f60c2d3df2812dbdd3c
rmsjayl <jaylord.ramos016@gmail.com>
2025-04-29 10:57:44 +0800

    [BE] Feature - Added Student Entity

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,5 +2,16 @@
 {
     public class Student
     {
+        public Guid Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public string? University { get; set; }
+        public string? CellPhoneNumber { get; set; }
+
+        protected static string GetName(string FirstName, string LastName)
+        {
+            return FirstName + " " + LastName;
+        }
     }
 }
