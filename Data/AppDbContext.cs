2510e38076e660ba37e3deb12c202db16708989b
rmsjayl <jaylord.ramos016@gmail.com>
2025-04-29 10:57:26 +0800

    [BE] Enhancement - Added DbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.Crud.Models;
+
+namespace WebApp.Crud.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+    }
+}
