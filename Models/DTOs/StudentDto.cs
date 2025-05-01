3871393037e1745c919c3c894d9b58405eb4f1c7
rmsjayl <jaylord.ramos016@gmail.com>
2025-05-01 08:03:46 +0800

    [BE] Fix - Added Guid Id to the DTO

--- Models/DTOs/StudentDto.cs
+++ Models/DTOs/StudentDto.cs
@@ -2,6 +2,7 @@
 {
     public class StudentDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string University { get; set; }
