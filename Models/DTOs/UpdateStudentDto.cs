ffbd1061f033f10042c6197229564db6491bb06c
rmsjayl <jaylord.ramos016@gmail.com>
2025-05-05 21:04:41 +0800

    [BE] Feature - Added Update Student Dto

---
+++ Models/DTOs/UpdateStudentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Crud.Models.DTOs
+{
+    public class UpdateStudentDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string University { get; set; } = string.Empty;
+        public string CellPhoneNumber { get; set; } = string.Empty;
+    }
+}
