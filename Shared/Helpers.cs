af9e4597859c21ade9eb07b14c144df67b64f53c
rmsjayl <jaylord.ramos016@gmail.com>
2025-05-07 23:08:48 +0800

    [BE] Fix - made new logic

---
+++ Shared/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using WebApp.Crud.Models.Domain;
+
+namespace WebApp.Crud.Shared
+{
+    public class Helpers
+    {
+        public void PropNullChecker(Student student)
+        {
+            var properties = student.GetType().GetProperties();
+            foreach (var property in properties)
+            {
+                var value = property.GetValue(student);
+
+                //if the value contains an empty string or null
+                if (value == null || string.IsNullOrEmpty(value.ToString()))
+                {
+                    throw new Exception($"Property {property.Name} cannot be null or empty.");
+                }
+            }
+        }
+    }
+}  
