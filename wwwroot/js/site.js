bb2896f535d4ea4fd193e60f148cb73eb28ea00c
rmsjayl <jaylord.ramos016@gmail.com>
2025-05-12 22:42:46 +0800

    [BE] Feature - Added Modal Show functionality

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function openModal() {
+    $("#studentModal").modal("show");
+    $().modal("hide");
+}
