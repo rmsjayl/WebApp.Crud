0675ee40a5428403ea0f0a55e8f7c687b2664df8
rmsjayl <jaylord.ramos016@gmail.com>
2025-05-15 12:01:00 +0800

    [FE] Fix - Renamed open modal show

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,7 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function addModal() {
+function openModal() {
     $("#studentModal").modal("show");
 };
 
