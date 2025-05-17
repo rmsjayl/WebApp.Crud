5cc74c0df0f88a9304a7a8a7a69d094a43653f4a
rmsjayl <jaylord.ramos016@gmail.com>
2025-05-17 11:38:45 +0800

    [FE] Fix - Added getstudent by ID

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,27 +1,45 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // Write your JavaScript code.
-function openModal() {
-    $("#studentModal").modal("show");
+var METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    DELETE: 'DELETE',
+    PUT: 'POST'
 };
 
-function editModal(studentId) {
-    fetch(`/api/Student/${studentId}`)
+
+function openModal() {
+    $("#studentModal").modal("show");
+}
+
+
+function getStudent(studentId) {
+    fetch(`/api/Student/${studentId}`, {
+        method: METHODS.GET,
+    })
         .then(response => response.json())
         .then(student => {
-            // Populate modal content dynamically
-            document.getElementById("modalBody").innerHTML = `
-                <p><strong>First Name:</strong> ${student.firstName}</p>
-                <p><strong>Last Name:</strong> ${student.lastName}</p>
-                <p><strong>University:</strong> ${student.university}</p>
-                <p><strong>Cell Phone:</strong> ${student.cellPhoneNumber}</p>
-                <button onclick="deleteStudent(${student.id})" class="btn btn-danger">Edit</button>
-            `;
+            console.log("Data: ", student);
+        })
+        .catch(error => console.log("Error fetching student:", error));
+}
 
-            // Show modal
+function editModal(studentId) {
+    fetch(`/api/Student/${studentId}`
+    )
+        .then(response => response.json())
+        .then(student => {
+            document.getElementById("modalBody").innerHTML = `
+            <p><strong>First Name:</strong> ${student.firstName}</p>
+            <p><strong>Last Name:</strong> ${student.lastName}</p>
+            <p><strong>University:</strong> ${student.university}</p>
+            <p><strong>Cell Phone:</strong> ${student.cellPhoneNumber}</p>
+            <button onclick="deleteStudent('${student.id}')" class="btn btn-danger">Edit</button>
+        `;
+
             let modal = new bootstrap.Modal(document.getElementById("studentModal"));
             modal.show();
         })
         .catch(error => console.log("Error fetching student:", error));
-};
+}
